b6c7bbb91d5a06a06016d730614951929c73e379
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-05-20 20:51:24 -0300

    (config): configuração do producer kafka

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
 
 func main() {
 	fmt.Println("Hello Go")
+}
+
+func newKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap {
+		"bootstrap.servers": "aplicacao-exemplo-kafka-go-fc-kafka-1:9092"
+	}
+
+	p, err := kafka.newProducer(configMap)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	return p
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/antonialucianapires/aplicacao-exemplo-kafka-go-fc
 
 go 1.16
+
+require github.com/confluentinc/confluent-kafka-go v1.8.2 // indirect
