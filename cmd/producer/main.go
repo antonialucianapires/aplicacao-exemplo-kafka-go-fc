f97a1dc81f76e8e0c7e59009055dc08095f27837
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-05-20 21:13:10 -0300

    (feature): envio de mensagens / publish producer

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
 )
 
 
 func main() {
-	fmt.Println("Hello Go")
+	producer := NewKafkaProducer()
+	Publish("ola gogo!", "gokafka", producer, nil)
+	producer.Flush(1000) //O Flush espera um retorno ou um tempo de milisegundos para encerrar o main
+	fmt.Println("Mensagem enviada!")
 }
 
-func newKafkaProducer() *kafka.Producer {
+func NewKafkaProducer() *kafka.Producer {
 	configMap := &kafka.ConfigMap {
-		"bootstrap.servers": "aplicacao-exemplo-kafka-go-fc-kafka-1:9092"
+		"bootstrap.servers": "aplicacao-exemplo-kafka-go-fc-kafka-1:9092",
 	}
 
-	p, err := kafka.newProducer(configMap)
+	p, err := kafka.NewProducer(configMap)
 
 	if err != nil {
 		log.Println(err.Error())
@@ -23,4 +27,17 @@ func newKafkaProducer() *kafka.Producer {
 
 	return p
 
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+	err := producer.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+	return nil
 }
